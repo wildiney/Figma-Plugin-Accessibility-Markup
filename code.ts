9d79285cb1be1aa3e51ccfd7e4f6ec1fc83800b7
Wildiney Di Masi <wildiney@gmail.com>
2024-10-04 18:05:46 -0300

    refactoring

--- code.ts
+++ code.ts
@@ -1,143 +1,137 @@
-let initialNumber = "1"
+let initialNumber = "1";
 
 figma.showUI(__html__, { width: 300, height: 400, themeColors: true });
-async function main () {
-  await figma.loadAllPagesAsync()
-}
-main()
 
-figma.ui.onmessage = async (msg: { type: string, marker?: string, value?: number, addSpace?: boolean }) => {
-  console.log("addSpace", msg.addSpace)
+figma.ui.onmessage = async (msg) => {
+  await figma.loadAllPagesAsync();
 
-  msg.marker != null ? console.log("marker", msg.marker) : null
+  const position: { [key: string]: string } = {
+    top_left: "Posição do marcador=↖️",
+    top_center: "Posição do marcador=↑",
+    top_right: "Posição do marcador=↗",
+    middle_left: "Posição do marcador=←",
+    middle_right: "Posição do marcador=→",
+    bottom_left: "Posição do marcador=↙",
+    bottom_center: "Posição do marcador=↓",
+    bottom_right: "Posição do marcador=↘",
+  };
 
-  if (msg.type === 'change-number') {
-    if (msg.value != null) {
-      const currentValue = msg.value
-      initialNumber = currentValue.toString()
-    }
-
+  switch (msg.type) {
+    case 'change-number':
+      handleNumberChange(msg.value);
+      break;
+    case 'insert-markup-frame':
+      await handleInsertMarkupFrame(msg, position);
+      break;
   }
-  if (msg.type === 'insert-markup-frame') {
-    const selectedFrame = figma.currentPage.selection[0]
-    let instanceName = null
-    switch (msg.marker) {
-      case "top_left":
-        instanceName = "Posição do marcador=↖️"
-        break
-      case "top_center":
-        instanceName = "Posição do marcador=↑"
-        break
-      case "top_right":
-        instanceName = "Posição do marcador=↗"
-        break
-      case "middle_left":
-        instanceName = "Posição do marcador=←"
-        break
-      case "middle_right":
-        instanceName = "Posição do marcador=→"
-        break
-      case "bottom_left":
-        instanceName = "Posição do marcador=↙"
-        break
-      case "bottom_center":
-        instanceName = "Posição do marcador=↓"
-        break
-      case "bottom_right":
-        instanceName = "Posição do marcador=↘"
-        break
-      default:
-        instanceName = "Posição do marcador=→"
+};
 
-    }
+function handleNumberChange (value: number | undefined) {
+  if (value != null) {
+    initialNumber = value.toString();
+  }
+}
 
-    if (
-      selectedFrame && selectedFrame.type === "FRAME" ||
-      selectedFrame && selectedFrame.type === "INSTANCE" ||
-      selectedFrame && selectedFrame.type === "TEXT") {
-      const componentSet = figma.root.findOne(node => node.name === "Specs-Component" && node.type === "COMPONENT_SET") as ComponentSetNode
-      if (componentSet) {
-        const variant = componentSet.findOne(node => node.name === instanceName && node.type === "COMPONENT") as ComponentNode
+async function handleInsertMarkupFrame (msg: { marker?: string; addSpace?: number }, position: { [key: string]: string }) {
+  const selectedFrame = figma.currentPage.selection[0];
 
-        if (variant) {
-          const instance = variant.createInstance()
-          const addSpaceValue = msg.addSpace == true ? 24 : 0
+  if (!selectedFrame) {
+    figma.notify("Nenhum frame ou instância selecionada");
+    return;
+  }
 
-          switch (msg.marker) {
-            case "top_left":
-              instance.x = selectedFrame.absoluteTransform[0][2] - addSpaceValue
-              instance.y = selectedFrame.absoluteTransform[1][2] - addSpaceValue
-              instance.resize(selectedFrame.width + addSpaceValue, selectedFrame.height + addSpaceValue)
-              break
-            case "top_center":
-              instance.x = selectedFrame.absoluteTransform[0][2]
-              instance.y = selectedFrame.absoluteTransform[1][2] - addSpaceValue
-              instance.resize(selectedFrame.width, selectedFrame.height + addSpaceValue)
-              break
-            case "top_right":
-              instance.x = selectedFrame.absoluteTransform[0][2]
-              instance.y = selectedFrame.absoluteTransform[1][2] - addSpaceValue
-              instance.resize(selectedFrame.width + addSpaceValue, selectedFrame.height + addSpaceValue)
-              break
-            case "middle_left":
-              instance.x = selectedFrame.absoluteTransform[0][2] - addSpaceValue
-              instance.y = selectedFrame.absoluteTransform[1][2]
-              instance.resize(selectedFrame.width + addSpaceValue, selectedFrame.height)
-              break
-            case "middle_right":
-              instance.x = selectedFrame.absoluteTransform[0][2]
-              instance.y = selectedFrame.absoluteTransform[1][2]
-              instance.resize(selectedFrame.width + addSpaceValue, selectedFrame.height)
-              break
-            case "bottom_left":
-              instance.x = selectedFrame.absoluteTransform[0][2] - addSpaceValue
-              instance.y = selectedFrame.absoluteTransform[1][2]
-              instance.resize(selectedFrame.width + addSpaceValue, selectedFrame.height + addSpaceValue)
-              break
-            case "bottom_center":
-              instance.x = selectedFrame.absoluteTransform[0][2]
-              instance.y = selectedFrame.absoluteTransform[1][2]
-              instance.resize(selectedFrame.width, selectedFrame.height + addSpaceValue)
-              break
-            case "bottom_right":
-              instance.x = selectedFrame.absoluteTransform[0][2]
-              instance.y = selectedFrame.absoluteTransform[1][2]
-              instance.resize(selectedFrame.width + addSpaceValue, selectedFrame.height + addSpaceValue)
-              break
+  const instanceName = msg.marker ? position[msg.marker] : null;
 
-          }
+  if (selectedFrame && ["FRAME", "INSTANCE", "TEXT"].includes(selectedFrame.type)) {
+    const componentSet = figma.root.findOne(node => node.name === "Specs-Component" && node.type === "COMPONENT_SET") as ComponentSetNode;
 
-          figma.currentPage.appendChild(instance)
-          const textNode = instance.findOne(node => node.type === "TEXT" && node.characters === "0") as TextNode
-          if (textNode) {
-            await figma.loadFontAsync(textNode.fontName as FontName)
-            const newText = initialNumber
-            textNode.characters = newText.toString()
-          }
+    if (componentSet) {
+      const variant = componentSet.findOne(node => node.name === instanceName && node.type === "COMPONENT") as ComponentNode;
 
-          figma.notify("Instância criada com sucesso")
-        } else {
-          figma.notify("Variante não encontrada")
+      if (variant) {
+        const instance = variant.createInstance();
+        const addSpaceValue = msg.addSpace || 0;
+
+        adjustInstancePosition(instance, selectedFrame, addSpaceValue, msg.marker);
+        figma.currentPage.appendChild(instance);
+
+        const textNode = instance.findOne(node => node.type === "TEXT" && node.characters === "0") as TextNode;
+
+        if (textNode) {
+          await figma.loadFontAsync(textNode.fontName as FontName);
+          textNode.characters = initialNumber;
         }
+        initialNumber = incrementNumber(initialNumber)
+
+        incrementNumber(initialNumber.toString())
+
+        figma.notify("Instância criada com sucesso");
       } else {
-        figma.notify("CompnentSet não encontrada")
+        figma.notify("Variante não encontrada");
       }
     } else {
-      figma.notify("Selecione um frame ou instância")
+      figma.notify("ComponentSet não encontrado");
     }
-    if (initialNumber.indexOf(".") === -1) {
-      initialNumber = (parseFloat(initialNumber) + 1).toString()
-    } else {
-      const splitedNumber = initialNumber.split(".")
-      console.log(splitedNumber)
-      const lastItem = parseInt(splitedNumber[splitedNumber.length - 1]) + 1
-      console.log(lastItem)
-      splitedNumber[splitedNumber.length - 1] = lastItem.toString()
-      console.log(splitedNumber[-1])
-      initialNumber = splitedNumber.join(".")
-    }
-    figma.ui.postMessage({ type: 'change-number', newValue: initialNumber });
+  }
+}
+
+function adjustInstancePosition (
+  instance: InstanceNode,
+  selectedFrame: FrameNode | InstanceNode | SceneNode,
+  addSpaceValue: number,
+  marker?: string
+) {
+  let xAdjust = 0;
+  let yAdjust = 0;
+  let shouldResizeWidth = true;
+  let shouldResizeHeight = true;
+
+  switch (marker) {
+    case "top_left":
+      xAdjust = -addSpaceValue;
+      yAdjust = -addSpaceValue;
+      break;
+    case "top_center":
+      yAdjust = -addSpaceValue;
+      shouldResizeWidth = false;
+      break;
+    case "top_right":
+      yAdjust = -addSpaceValue;
+      break;
+    case "middle_left":
+      xAdjust = -addSpaceValue;
+      shouldResizeHeight = false; // Não ajusta a altura
+      break;
+    case "middle_right":
+      shouldResizeHeight = false; // Não ajusta a altura
+      break;
+    case "bottom_left":
+      xAdjust = -addSpaceValue;
+      break;
+    case "bottom_center":
+      shouldResizeWidth = false; // Não ajusta a largura
+      break;
+    case "bottom_right":
+      break;
   }
 
+  instance.x = selectedFrame.absoluteTransform[0][2] + xAdjust;
+  instance.y = selectedFrame.absoluteTransform[1][2] + yAdjust;
 
-};
+  // Só redimensiona se for necessário
+  const newWidth = shouldResizeWidth ? selectedFrame.width + addSpaceValue : selectedFrame.width;
+  const newHeight = shouldResizeHeight ? selectedFrame.height + addSpaceValue : selectedFrame.height;
+
+  instance.resize(newWidth, newHeight);
+}
+
+function incrementNumber (number: string): string {
+  if (number.indexOf(".") === -1) {
+    return (parseFloat(number) + 1).toString();
+  } else {
+    const splitedNumber = number.split(".");
+    const lastItem = parseInt(splitedNumber[splitedNumber.length - 1]) + 1;
+    splitedNumber[splitedNumber.length - 1] = lastItem.toString();
+    return splitedNumber.join(".");
+  }
+}
